1d750dffbbd64bbe9daf477a9a29047dd0c2960f
Axel Hernández <axeelhrz@gmail.com>
2025-08-22 22:54:32 -0300

    fff

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,9 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://web-production-40b3.up.railway.app/api/:path*', // 👈 con https:// y /api/:path*
+        destination: 'https://web-production-40b3.up.railway.app/api/:path*',
       },
     ];
   },
 };
-
 module.exports = nextConfig;
